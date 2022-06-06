0b093990950abb3dbb5917e3c2144bbf2fc992f3
Eric-Li323 <liyonghuiwangyi@163.com>
2022-06-06 14:59:05 +0800

    首页UI布局

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {Button, Radio} from 'element-ui'
+import {Button, Radio, Container, Main, Header, Aside} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 
 import router from '../router'
@@ -8,6 +8,11 @@ import router from '../router'
 Vue.config.productionTip = false
 Vue.use(Button)
 Vue.use(Radio)
+
+Vue.use(Container)
+Vue.use(Main)
+Vue.use(Header)
+Vue.use(Aside)
 
 new Vue({
   router,
--- views/Home.vue
+++ views/Home.vue
@@ -1,11 +1,25 @@
 <template>
-    <div>我是Home页面</div>
+  <el-container style = "height: 100%">
+    <el-aside width="auto">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
-    name: 'Home',
-    data () {
-        return {}
-    }
-}
+  name: "Home",
+  data() {
+    return {};
+  },
+};
 </script>
+<style lang="less" scope>
+.el-header {
+    background-color: #333;
+}
+.el-main {
+    padding-top: 0;
+}
+</style>
