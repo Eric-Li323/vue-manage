b9492bdae3cd9ccb449421239b575a44ab81faa4
Eric-Li323 <liyonghuiwangyi@163.com>
2022-06-06 14:12:53 +0800

    路由搭建

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import {Button, Radio} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 
+import router from '../router'
+
 Vue.config.productionTip = false
 Vue.use(Button)
 Vue.use(Radio)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
