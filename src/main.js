cd90709223781f9b7258bdfe830265b6821f61aa
Eric-Li323 <liyonghuiwangyi@163.com>
2022-06-09 14:40:33 +0800

    home页 登录卡片

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import {Button, Radio, Container, Main, Header, Aside, Menu,Submenu, MenuItem, MenuItemGroup, Dropdown,DropdownMenu,DropdownItem} from 'element-ui'
+import {Button, Radio, Container, Main, Header, Aside, Menu,Submenu, MenuItem, MenuItemGroup,
+ Dropdown,DropdownMenu,DropdownItem,
+ Row, Col,Card
+ } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/less/index.less'
 
@@ -24,6 +27,9 @@ Vue.use(Dropdown)
 Vue.use(DropdownMenu)
 Vue.use(DropdownItem)
 
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Card)
 
 new Vue({
   store,
